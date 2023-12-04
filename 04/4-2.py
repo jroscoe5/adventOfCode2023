4f93e7039fe00747bc72061b357a34dc0cb2fb2c
Jonathon Roscoe <36338176+jroscoe5@users.noreply.github.com>
2023-12-04 11:37:55 -0800

    Update 4-2.py

--- 04/4-2.py
+++ 04/4-2.py
@@ -3,19 +3,14 @@
 with open('input.txt') as f:
     cards = [line for line in f.readlines()]
     counts = [1 for _ in cards]
-    lookup = {}
 
     for i in range(len(cards)):
-        for j in range(counts[i]):
-            if i in lookup:
-                wins = lookup[i]
-            else:
-                numbers = cards[i].split(':')[1].strip()
-                winners = set([n for n in numbers.split('|')[0].strip().split(' ') if n != ''])
-                havers = set([n for n in numbers.split('|')[1].strip().split(' ') if n != ''])
-                lookup[i] = wins = len(winners & havers)
+        numbers = cards[i].split(':')[1].strip()
+        winners = set([n for n in numbers.split('|')[0].strip().split(' ') if n != ''])
+        havers = set([n for n in numbers.split('|')[1].strip().split(' ') if n != ''])
+        wins = len(winners & havers)
 
-            for k in range(i + 1, i + 1 + wins):
-                counts[k] += 1 
+        for k in range(i + 1, i + 1 + wins):
+            counts[k] += counts[i]
 
     print(sum(counts))
